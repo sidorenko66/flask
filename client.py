51402e126880244dd646ed3b7623367eaeab9850
sidorenko66 <105962938+sidorenko66@users.noreply.github.com>
2023-02-07 22:45:03 +0500

    Update client.py

--- client.py
+++ client.py
@@ -1,10 +1,10 @@
 import requests
 
-response = requests.get('http://127.0.0.1:5001')
+response = requests.get('http://127.0.0.1:5001/users/1')
 print(response.status_code)
 print(response.text)
 
-response = requests.post('http://127.0.0.1:5001',
+response = requests.post('http://127.0.0.1:5001/users',
                          json={'email': 'user1@usermail.ru', 'password': '1234'})
 print(response.status_code)
 print(response.text)
